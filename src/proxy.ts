6d35a596943d709f618ea662243ea90ad944dbb6
Joaquin Hernandez <joaquinhernandezph@gmail.com>
2026-09-12 11:11:00 -0300

    fix: proxy matcher including icon

--- src/proxy.ts
+++ src/proxy.ts
@@ -12,5 +12,7 @@ export const proxy = auth((request) => {
 });
 
 export const config = {
-  matcher: ['/((?!api/auth|_next/static|_next/image|icon.png).*)'],
+  matcher: [
+    '/((?!api/auth|_next/static|_next/image|favicon.ico|robots.txt|sitemap|manifest.webmanifest|(?:.+/)?(?:icon|apple-icon|opengraph-image|twitter-image)\\d*$).*)',
+  ],
 };
