a448d82f93be8f0d6262c4f25852070fc60e40eb
Martin Antunez <mantunez0108@gmail.com>
2026-09-19 09:47:32 -0300

    feat(projects): add skeleton

--- src/app/(protected)/dashboard/projects/page.tsx
+++ src/app/(protected)/dashboard/projects/page.tsx
@@ -1,12 +1,11 @@
+import { Suspense } from 'react';
 import { redirect } from 'next/navigation';
 import prisma from '@/lib/prisma';
 import { auth } from '@/auth';
 import { ProjectsCardList } from '@/components/projects-card-listing';
+import { ProjectsCardListSkeleton } from '@/components/projects-card-listing-skeleton';
 
-export default async function ProjectsPage() {
-  const session = await auth();
-  if (!session?.user) redirect('/login');
-
+async function Projects() {
   const projects = await prisma.project.findMany({
     include: {
       leadCoordinator: { select: { firstName: true, lastName: true } },
@@ -17,4 +16,15 @@ export default async function ProjectsPage() {
   });
 
   return <ProjectsCardList projects={projects} />;
+}
+
+export default async function ProjectsPage() {
+  const session = await auth();
+  if (!session?.user) redirect('/login');
+
+  return (
+    <Suspense fallback={<ProjectsCardListSkeleton />}>
+      <Projects />
+    </Suspense>
+  );
 }
